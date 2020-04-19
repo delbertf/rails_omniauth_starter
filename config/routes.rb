4b7a9c00f21676e53c28a78475d188c790740f51
dfriesen <friesen.delbert@gmail.com>
2020-04-19 18:17:23 +0200

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get   '/login', :to => 'sessions#new', :as => :login
-	match '/auth/:provider/callback', :to => 'sessions#create'
-	match '/auth/failure', :to => 'sessions#failure'
+	get '/login', :to => 'sessions#new', :as => :login
+	get '/auth/:provider/callback', :to => 'sessions#create'
+	get '/auth/failure', :to => 'sessions#failure'
 end
