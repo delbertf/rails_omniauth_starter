251d24f6c53ec5a7af5e7936759eba7190f456a6
dfriesen <friesen.delbert@gmail.com>
2020-04-19 18:16:53 +0200

    add twitter credentials and auth-provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, Rails.application.credentials.facebook[:access_app_id], Rails.application.credentials.facebook[:secret_access_key]
+	provider :twitter, Rails.application.credentials.twitter[:access_app_id], Rails.application.credentials.twitter[:secret_access_key]
 end
