727a1b6c5137e8b72cc780226476c75773929a2d
dfriesen <friesen.delbert@gmail.com>
2020-04-19 16:48:54 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/failure'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get   '/login', :to => 'sessions#new', :as => :login
+	match '/auth/:provider/callback', :to => 'sessions#create'
+	match '/auth/failure', :to => 'sessions#failure'
 end
