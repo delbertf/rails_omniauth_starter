fd5480db68da3188e7a5e7480c5f33202985280b
dfriesen <friesen.delbert@gmail.com>
2020-04-19 18:15:20 +0200

    refactor session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	auth_hash = request.env['omniauth.auth']
+
+  	if session[:user_id]
+  		# Means our user is signed in. Add the authorization to the user
+  		User.find(session[:user_id]).add_provider(auth_hash)
+
+  		render :plain => "You can now login using #{auth_hash["provider"].capitalize} too!"
+		else
+			# Log him in or sign him up
+			auth = Authorization.find_or_create(auth_hash)
+
+			# create the session
+			session[:user_id] = auth.user.id
+
+			render :plain => "Welcome #{auth.user.name}!"
+		end
   end
 
   def failure
